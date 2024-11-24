71512ffaf6083c5fb2f6fbe1d57267f454bce9f1
David Hejtmánek <david07@post.cz>
2024-11-24 16:02:25 +0100

    Mé citace

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header className="header">
+        <div className="logo">David Hejtmánek - Básník a autor knih</div>
       </header>
+
+      <main>
+        <section id="quotes" className="content">
+          <h1>Citace z knih a článků známého autora Davida Hejtmánka</h1>
+
+          <div className="quotes-container">
+            <div className="quote">
+              <p>"Výsledek pro všechny matematické rovnice je různý, ale většina z nich odpovídá konstantě 2-CB." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Myslel jsem si, že své jméno znám moc dobře, ale zjistil jsem, že Traq bylo mé jméno odjakživa." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Jakmile jsem ochutnal první zápis, už nikdy jsem si nebyl jistý, co znamená „vstupní bod“, ale „jenkem“ se mi vryl do paměti." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Proč by měl být problém něco změnit, když durman vždycky ve všem najde cestu?" - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Jsem si jistý, že když se podívám na život skrze čočku, všechno začne dávat smysl… DPT jsem vyzkoušel, a věci se začaly skládat." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Pokud si myslíte, že tahle planeta je jedním velkým zrcadlem, asi jste ještě nikdy neviděli souhvězdí jako Tucibi." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"V mé minulosti jsem se s únavou potýkal často, ale když jsem se setkal s Rohypnolem, museli jsme jí dát 'pá pá'" - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Ten pocit, když všechny vchody do neznámého prostoru jsou najednou uzavřeny, ale Sisa najde vždy cestu ven." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Byl jsem na křižovatce, když jsem si uvědomil, že něco jako Desoxipipradrol mi otevřelo dveře do nových možností. Nic už nebylo jako dřív." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Začal jsem chápat, že existují různé cesty, než jen ta, která vede k 'různým typům vnímání'. Když to začnete správně vnímat, všechno se propojí – To je 4F-PVP." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>""Co se vlastně stane, když vědecký výzkum zcela změní definici vnímání? Pak přichází Pentedron." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Po pár letech jsem zjistil, že nejvlastnější poznání je, že neexistuje žádná konečná odpověď, jen jako Chlorpromazin, co se nikdy nezeptá na smysl života." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Dokonce i moje vzpomínky na každý den se začaly měnit, když jsem si uvědomil, že většina z nich je jen produkt „Space Rock“." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Nemusím mít rád všechny věci v životě, abych mohl najít vděčnost v jednom drobném momentu. Takový je přístup k „Zenith“." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Když jsem poprvé slyšel o 'Bromo-DragonFLY', myslel jsem, že jde o nějaký nový produkt pro začátečníky v IT, ale když jsem to zkoumal, všechno mi začalo dávat smysl." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>""Po celé roky jsem si myslel, že klíčem k životu je jednoduchost, ale pak jsem narazil na 'MXE' - technickou rovnici a najednou začalo všechno dávat smysl." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Pochopení, že k těmto jevům neexistuje žádné slovo, které by vám změnilo pohled na techniku v IT průmyslu, jsem se zasmál když jsem slyšel o programu fencyklidinu." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Hledání významu v chaosu nápojů, mi připomíná okamžiky, kdy jsem poprvé poznal nápoj jahodový nápoj známý jako „Ajahaska“." - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Nikdy jsem nečekal, že MDAI, bude mít takový význam v IT, a podívejte se na nás dneska používáme tento program denně!" - David Hejtmánek</p>
+            </div>
+            <div className="quote">
+              <p>"Když jsem poprvé zkoumal, jak se věci mění, myslel jsem si, že jsem objevil nový vzorec. Ale když jsem potkal program Meskalit, zjistil jsem, že vzory jsou v počítači, ne na papíře." - David Hejtmánek</p>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer className="footer">
+        <p>"Svět není takový, jaký se nám jeví. A možná, že nikdy nepochopíme, co vlastně znamená být skutečně přítomný." - David Hejtmánek</p>
+      </footer>
     </div>
   );
 }
